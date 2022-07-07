ba485b96c43991509684cdd4fc32a6e9e63e7f6e
Peter Lewis <plewis@calibrateconsulting.com>
2022-07-07 12:19:57 +0100

    changes

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE'),
-      user: env('DATABASE_USER'),
-      password: env('DATABASE_PASSWORD'),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
         rejectUnauthorized: false
       },
